af335fc03bfd8fff20ec424560151ca1656a389b
chsc0817 <chris1303@web.de>
2016-06-02 12:51:48 +0200

    Fehlerausgabe bei Load jetzt ueber panic

--- src/csgo/Load.go
+++ src/csgo/Load.go
@@ -22,8 +22,7 @@ func (rl *Relation) Load(csvFile string, separator rune) {
 
 	//Error if file could not be opened or found
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		panic(err)
 	}
 
 	reader := csv.NewReader(file)
@@ -32,8 +31,7 @@ func (rl *Relation) Load(csvFile string, separator rune) {
 
 	//File hat not the same number of Columns than the Relation.
 	if len(record) != len(rl.Columns) {
-		fmt.Println("Number of defined columns is different than the number of columns in ", path.Base(csvFile))
-		os.Exit(1)
+		panic(fmt.Sprintf("Number of defined columns is different than the number of columns in "+path.Base(csvFile)))
 	}
 	//Create the data slices and insert the first row
 	for i := 0; i < len(record); i++ {
@@ -43,12 +41,8 @@ func (rl *Relation) Load(csvFile string, separator rune) {
 			datas, err := strconv.Atoi(record[i])
 
 			//Error if inputType is not int
-			if err != nil {
-				fmt.Println("error while loading", path.Base(csvFile))
-				fmt.Print("row ", len(rl.Columns[i].Data.([]int))+1, ", column ", i+1, ": ")
-				fmt.Print("\"", record[i], "\"")
-				fmt.Println(" is not type int")
-				os.Exit(1)
+			if err != nil {			
+				panic(fmt.Sprintf("error while loading "+path.Base(csvFile)+"\n"+"row "+strconv.Itoa(len(rl.Columns[i].Data.([]int))+1)+", column "+strconv.Itoa(i+1)+": "+"\n"+fmt.Sprintf(record[i])+" is not type int"))
 			}
 			rl.Columns[i].Data = append(rl.Columns[i].Data.([]int), datas)
 
@@ -58,11 +52,7 @@ func (rl *Relation) Load(csvFile string, separator rune) {
 
 			//Error if inputType is not float
 			if err != nil {
-				fmt.Println("error while loading", path.Base(csvFile))
-				fmt.Print("row ", len(rl.Columns[i].Data.([]int))+1, ", column ", i+1, ": ")
-				fmt.Print("\"", record[i], "\"")
-				fmt.Println(" is not type float")
-				os.Exit(1)
+				panic(fmt.Sprintf("error while loading "+path.Base(csvFile)+"\n"+"row "+strconv.Itoa(len(rl.Columns[i].Data.([]int))+1)+", column "+strconv.Itoa(i+1)+": "+"\n"+fmt.Sprintf(record[i])+" is not type float"))
 			}
 			rl.Columns[i].Data = append(rl.Columns[i].Data.([]float64), datas)
 
@@ -82,8 +72,7 @@ func (rl *Relation) Load(csvFile string, separator rune) {
 
 		//Error if file could not be read
 		if err != nil {
-			fmt.Print(err)
-			os.Exit(1)
+			panic(err)
 		}
 
 		for i := 0; i < len(record); i++ {
@@ -94,11 +83,7 @@ func (rl *Relation) Load(csvFile string, separator rune) {
 
 				//Error if inputType is not int
 				if err != nil {
-					fmt.Println("error while loading", path.Base(csvFile))
-					fmt.Print("row ", len(rl.Columns[i].Data.([]int))+1, ", column ", i+1, ": ")
-					fmt.Print("\"", record[i], "\"")
-					fmt.Println(" is not type int")
-					os.Exit(1)
+					panic(fmt.Sprintf("error while loading "+path.Base(csvFile)+"\n"+"row "+strconv.Itoa(len(rl.Columns[i].Data.([]int))+1)+", column "+strconv.Itoa(i+1)+": "+"\n"+fmt.Sprintf(record[i])+" is not type int"))
 				}
 				rl.Columns[i].Data = append(rl.Columns[i].Data.([]int), datas)
 
@@ -107,11 +92,7 @@ func (rl *Relation) Load(csvFile string, separator rune) {
 
 				//Error if inputType is not float
 				if err != nil {
-					fmt.Println("error while loading", path.Base(csvFile))
-					fmt.Print("row ", len(rl.Columns[i].Data.([]int))+1, ", column ", i+1, ": ")
-					fmt.Print("\"", record[i], "\"")
-					fmt.Println(" is not type float")
-					os.Exit(1)
+					panic(fmt.Sprintf("error while loading "+path.Base(csvFile)+"\n"+"row "+strconv.Itoa(len(rl.Columns[i].Data.([]int))+1)+", column "+strconv.Itoa(i+1)+": "+"\n"+fmt.Sprintf(record[i])+" is not type float"))
 				}
 				rl.Columns[i].Data = append(rl.Columns[i].Data.([]float64), datas)
 
